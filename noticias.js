e92ae557253275fb9304af8307c34b81084f7512
Marcelo Tomporoski Perez <marcelotperez@gmail.com>
2024-02-08 10:38:39 -0400

    criação de packege e update noticias

--- noticias.js
+++ noticias.js
@@ -1,6 +1,16 @@
 var http = require('http');
-var server = http.createServer(function(req,res){
-    res.end("<html> <body> Primeiro projeto Node - HTML </body></html>")
+var server = http.createServer(function (req, res) {
+
+    var categoria = req.url;
+    if (categoria == '/cursos') {
+        res.end("<html> <body>IFMS Not&iacutecias de Cursos</body></html>");
+    } else if (categoria == '/esportes') {
+        res.end("<html> <body> IFMS - Not&iacutecias de Esportes</body></html>");
+    } else if (categoria == '/pesquisa') {
+        res.end("<html> <body> IMFS - Not&iacutecias de Pesquisas</body></html>");
+    } else {
+        res.end("<html> <body> Portal IFMS Not&iacutecias</body></html>");
+    }
 });
 
 server.listen(3000);
