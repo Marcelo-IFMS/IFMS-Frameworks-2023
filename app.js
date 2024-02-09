6b67b77507a00fd4069791b99207b5fa2ceade0a
Marcelo Tomporoski Perez <marcelotperez@gmail.com>
2024-02-09 10:14:38 -0400

    Alterado as rotas

--- app.js
+++ app.js
@@ -1,23 +1,13 @@
 var app = require('./config/server');
 
-app.get('/',function(req,res){
-    res.render('home/index');
-});
-app.get('/cursos',function(req,res){
-    res.render("secao/cursos");
-});
-app.get('/esportes',function(req,res){
-    res.render("secao/esportes");
-});
-app.get('/pesquisa',function(req,res){
-    res.render("secao/pesquisa");
-});
-app.get('/formulario_inclusao_noticias',function(req,res){
-    res.render("admin/form_add_noticia");
-});
-app.get('/noticias',function(req,res){
-    res.render("noticias/noticias");
-});
+var rotaNoticias = require('./app/routes/noticias');
+rotaNoticias(app);
+
+var rotaHome = require('./app/routes/home');
+rotaHome(app);
+
+var rotaFormInclusaoNoticia = require('./app/routes/formulario_inclusao_noticias');
+rotaFormInclusaoNoticia(app);
 
 app.listen(3000, function(){
     console.log('Servidor ON');
