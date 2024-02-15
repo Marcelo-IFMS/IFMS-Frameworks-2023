d5f8d4fdedf0443bf9e4007dbef88bf09f5dd2d4
Marcelo Tomporoski Perez <marcelotperez@gmail.com>
2024-02-15 10:43:30 -0400

    criado a conexão com banco

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,5 +1,17 @@
 module.exports = function(app){
     app.get('/noticias', function(req,res){
-        res.render("noticias/noticias");
+        var mysql = require ('mysql');
+        var connection = mysql.createConnection({
+            host: 'localhost',
+            user: 'ifms',
+            password: 'ifms2024!',
+            database:'portal_noticias'
+        })
+        
+        connection.query('select * from noticias', function(error,result){
+            res.render("noticias/noticias",{noticias: result});
+        });
+        
+        //res.render("noticias/noticias");
     })
 }
